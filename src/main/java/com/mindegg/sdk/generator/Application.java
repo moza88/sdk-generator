d8980e0215d685cbd9b2fd7b492922146c66acc7
Mabel Oza <ozamabel@gmail.com>
2023-03-27 04:31:09 +0000

    Initial commit

---
+++ src/main/java/com/mindegg/sdk/generator/Application.java
@@ -0,0 +1,10 @@
+package com.mindegg.sdk.generator;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
