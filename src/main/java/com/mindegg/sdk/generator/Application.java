53ab72c5d69a70bbab76bbbccd7e558ae983ec3b
Mabel Oza <ozamabel@gmail.com>
2023-03-27 00:55:48 -0500

    init: initalizing repo

--- src/main/java/com/mindegg/sdk/generator/Application.java
+++ src/main/java/com/mindegg/sdk/generator/Application.java
@@ -1,10 +1,18 @@
 package com.mindegg.sdk.generator;
 
+import com.mindegg.sdk.generator.controller.PetsApi;
+import com.mindegg.sdk.generator.invoker.ApiClient;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws com.mindegg.sdk.generator.invoker.ApiException {
         Micronaut.run(Application.class, args);
+
+        ApiClient apiClient = new ApiClient();
+
+        PetsApi petsApi = new PetsApi(apiClient);
+
+        petsApi.listPets(2);
     }
 }
